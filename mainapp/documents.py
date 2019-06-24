04f51273392d5ac062f7c55f7ceebd5313ebc3f7
Yakunin Kirill <yakunin.k@mail.ru>
2019-06-24 14:42:06 +0600

    ElasticSearch-dsl Document

---
+++ mainapp/documents.py
@@ -0,0 +1,25 @@
+import elasticsearch_dsl as es
+
+
+class Document(es.Document):
+    corpus = es.Keyword()
+    source = es.Keyword()
+    author = es.Keyword()
+    title = es.Text(fields={'keyword': es.Keyword()})
+    text = es.Text()
+    html = es.Text()
+    links = es.Keyword()
+    url = es.Keyword()
+
+    datetime = es.Date()
+    datetime_parsed = es.Date()
+    datetime_indexed = es.Date()
+    datetime_modified = es.Date()
+
+    num_views = es.Integer()
+    num_shares = es.Integer()
+    num_comments = es.Integer()
+    num_likes = es.Integer()
+
+    tags = es.Keyword()
+    categories = es.Keyword()
