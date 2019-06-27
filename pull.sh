5545bcc5014eec57e12a82d433aab08616cb31d9
Yakunin Kirill <yakunin.k@mail.ru>
2019-06-27 16:21:56 +0600

    Attempt to build own MariaDB-ColumnStore (Unicode issues)

--- pull.sh
+++ pull.sh
@@ -2,3 +2,4 @@
 set -e
 docker login 109.233.109.111:5000
 docker pull 109.233.109.111:5000/nlpmonitor:prod
+docker pull 109.233.109.111:5000/mariadb-columnstore:prod
--- push.sh
+++ push.sh
@@ -4,3 +4,9 @@ cd web
 docker build . -f docker/Dockerfile -t 109.233.109.111:5000/nlpmonitor:prod
 docker login 109.233.109.111:5000
 docker push 109.233.109.111:5000/nlpmonitor:prod
+
+cd ..
+cd mariadb-columnstore
+docker build . -f Dockerfile -t 109.233.109.111:5000/mariadb-columnstore:prod
+docker login 109.233.109.111:5000
+docker push 109.233.109.111:5000/mariadb-columnstore:prod
