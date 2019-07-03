08a7a83dc3d106dbcd6fce972a779571a66dc706
Yakunin Kirill <yakunin.k@mail.ru>
2019-07-03 15:37:08 +0600

    Refactor Service_ES - field groups names consistency

--- web/mainapp/services_es_documents.py
+++ web/mainapp/services_es_documents.py
@@ -2,16 +2,16 @@ from elasticsearch_dsl import Search, Q
 from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENTS
 
 
-FILTER_FIELDS = (('corpuses', 'corpus'),
-                 ('sources', 'source'),
-                 ('authors', 'author'),
-                 ('tags', 'tags'),
-                 ('categories', 'categories'), )
-FILTER_FIELDS_DICT = dict(FILTER_FIELDS)
-MUST_FIELDS = ('title', 'id')
+TERM_FIELDS = (('corpuses', 'corpus'),
+               ('sources', 'source'),
+               ('authors', 'author'),
+               ('tags', 'tags'),
+               ('categories', 'categories'),)
+TERMS_FIELDS_DICT = dict(TERM_FIELDS)
+MATCH_FIELDS = ('title', 'id')
 MULTI_FIELDS = ('text', )
-FROM_FIELDS = ('datetime_from', 'num_views_from', 'num_shares_from', 'num_comments_from', 'num_likes_from', )
-TO_FIELDS = ('datetime_to', 'num_views_to', 'num_shares_to', 'num_comments_to', 'num_likes_to', )
+RANGE_FROM_FIELDS = ('datetime_from', 'num_views_from', 'num_shares_from', 'num_comments_from', 'num_likes_from',)
+RANGE_TO_FIELDS = ('datetime_to', 'num_views_to', 'num_shares_to', 'num_comments_to', 'num_likes_to',)
 
 SOURCE_FIELDS = ('id', 'datetime', 'title', 'source', )
 
@@ -19,7 +19,7 @@ SOURCE_FIELDS = ('id', 'datetime', 'title', 'source', )
 def es_filter(search, key, value):
     query = 'terms'
     value = list([v.name for v in value])
-    key = FILTER_FIELDS_DICT[key]
+    key = TERMS_FIELDS_DICT[key]
     return search.filter(query, **{key: value})
 
 
@@ -30,27 +30,23 @@ def execute_search(val_data):
     for key, value in val_data.items():
         if not value:
             continue
-        if key in [f[0] for f in FILTER_FIELDS]:
+        if key in TERMS_FIELDS_DICT:
             s = es_filter(s, key, value)
-        elif key in MUST_FIELDS:
-            if isinstance(value, list):
-                value = value[0]
-            if value not in ('', None):
-                s = s.query('match', **{key: value})
+        elif key in MATCH_FIELDS:
+            s = s.query('match', **{key: value})
         elif key in MULTI_FIELDS:
             if isinstance(value, list):
                 value = value[0]
-            if value not in ('', None):
-                q = Q('multi_match',
-                      query=value,
-                      fields=['title^10',
-                              'tags^3',
-                              'categories^3',
-                              'text^2'])
-                s = s.query(q)
-        elif key in TO_FIELDS:
+            q = Q('multi_match',
+                  query=value,
+                  fields=['title^10',
+                          'tags^3',
+                          'categories^3',
+                          'text^2'])
+            s = s.query(q)
+        elif key in RANGE_TO_FIELDS:
             s = s.filter('range', **{key.replace("_to", ""): {'lte': value}})
-        elif key in FROM_FIELDS:
+        elif key in RANGE_FROM_FIELDS:
             s = s.filter('range', **{key.replace("_from", ""): {'gte': value}})
 
     s = s[:1000]
