bd5544d68676fc70eb0faa40a1dd0c8b63dd1af7
Yakunin Kirill <yakunin.k@mail.ru>
2019-07-04 12:31:57 +0600

    Delete previous data on build_dashboard

--- web/mainapp/management/commands/build_dashboard.py
+++ web/mainapp/management/commands/build_dashboard.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from elasticsearch_dsl import Search
 
 from mainapp.models import *
 from mainapp.models import Document as ModelDocument
@@ -9,10 +10,19 @@ from mainapp.services import batch_qs, date_generator
 class Command(BaseCommand):
     def handle(self, *args, **options):
         self.tag_batch_size = 10000
-        index = es.Index(ES_INDEX_DASHOBARD, using=ES_CLIENT)
-        index.delete(ignore=404)
+        # index = es.Index(ES_INDEX_DASHOBARD, using=ES_CLIENT)
+        # index.delete(ignore=404)
         Dashboard.init()
         self.build_publications_by_tag()
+
+    def delete_ready_dashboards(self, type, tag=None):
+        s = Search(using=ES_CLIENT, index=ES_INDEX_DASHOBARD)
+        s = s.params(conflicts='proceed')
+        s = s.filter("term", **{"type": type})
+        s = s.filter("term", **{"is_ready": True})
+        if tag:
+            s = s.filter("term", **{"tag": tag})
+        s.delete()
 
     def build_publications_by_tag(self):
         for corpus in Corpus.objects.all():
@@ -30,4 +40,5 @@ class Command(BaseCommand):
                         dashboard_document.add_value(ds.filter(datetime__contains=date).count(), date)
                     dashboard_document.datetime_generated = timezone.now()
                     dashboard_document.is_ready = True
+                    self.delete_ready_dashboards(DASHBOARD_TYPE_NUM_PUBLICATIONS_BY_TAG, tag.name)
                     dashboard_document.save()
