788862f2e1b5137bdae0ef5f04c7bd3753940637
Yakunin Kirill <yakunin.k@mail.ru>
2019-08-29 17:39:28 +0600

    Backup for airflow-db

--- db_backup.sh
+++ db_backup.sh
@@ -2,6 +2,3 @@
 cd /var/www/nlpmonitor
 /usr/local/bin/docker-compose exec -T db chmod 777 /bin/db_backup.sh
 /usr/local/bin/docker-compose exec -T db /bin/db_backup.sh
-
-/usr/local/bin/docker-compose exec -T airflow-postgres chmod 777 /bin/db_backup.sh
-/usr/local/bin/docker-compose exec -T airflow-postgres /bin/db_backup.sh
