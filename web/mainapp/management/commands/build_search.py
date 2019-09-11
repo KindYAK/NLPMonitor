740f7494b1d0f73ea1385432d6c19161a537f0ea
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-11 12:51:42 +0600

    Build_search - start_from argument

--- web/mainapp/management/commands/build_search.py
+++ web/mainapp/management/commands/build_search.py
@@ -14,13 +14,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.batch_size = options['batch_size']
+        self.start_from = 0
+        if "start_from" in options:
+            self.start_from = options['start_from']
         self.client = ES_CLIENT
-        print("Deleting index")
-        index = es.Index(ES_INDEX_DOCUMENT, using=self.client)
-        index.delete(ignore=404)
-        print("Creating index")
-        ESDocument.init()
-        self.parse_csv()
+        if not self.start_from:
+            print("Deleting index")
+            index = es.Index(ES_INDEX_DOCUMENT, using=self.client)
+            index.delete(ignore=404)
+            print("Creating index")
+            ESDocument.init()
+            self.parse_csv()
 
     def document_generator(self, qs):
         for batch in batch_qs(qs, batch_size=self.batch_size):
@@ -32,7 +36,7 @@ class Command(BaseCommand):
     def parse_csv(self):
         success = 0
         failed = 0
-        qs = Document.objects.all()
+        qs = Document.objects.filter(id__gte=self.start_from)
         print("Start build")
         for ok, result in streaming_bulk(self.client, self.document_generator(qs), index=ES_INDEX_DOCUMENT, chunk_size=self.batch_size, raise_on_error=False, max_retries=10):
             if ok:
