48c94d82b1d74cfa50f1d3d1781045699a89b88f
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-12 11:06:16 +0600

    One-timer script - small fix

--- scrpits (one timers)/tengrinews-Irina Maratovna/output_tengrin_stuff.py
+++ scrpits (one timers)/tengrinews-Irina Maratovna/output_tengrin_stuff.py
@@ -11,10 +11,10 @@ for d in ds:
         "text": d.text,
         "type": dict(Document.TYPES)[d.type],
         "url": d.url,
-        "datetime": d.datetime,
+        "datetime": d.datetime.isoformat(),
         "number_of_img": d.html.count("<img"),
         "number_of_paragraphs": d.html.count("<p")
     })
 
-with open("/backup/output-IM.json", "w", encoding="utf-8") as f:
+with open("/output-IM.json", "w", encoding="utf-8") as f:
     f.write(json.dumps(output))
