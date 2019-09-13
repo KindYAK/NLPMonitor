467893fb5a5dcf9d0bf706c93db7430a4ae6f4ac
Yakunin Kirill <yakunin.k@mail.ru>
2019-09-13 11:12:51 +0600

    Small fixes

--- scrpits (one timers)/tengrinews-Irina Maratovna/output_tengrin_stuff.py
+++ scrpits (one timers)/tengrinews-Irina Maratovna/output_tengrin_stuff.py
@@ -2,7 +2,7 @@ import json
 
 from mainapp.models import Document
 
-ds = Document.objects.filter(source__id=9, type__gte=1)
+ds = Document.objects.filter(source__id=9, type=0)
 
 output = []
 for d in ds:
@@ -11,9 +11,9 @@ for d in ds:
         "text": d.text,
         "type": dict(Document.TYPES)[d.type],
         "url": d.url,
-        "datetime": d.datetime.isoformat(),
-        "number_of_img": d.html.count("<img"),
-        "number_of_paragraphs": d.html.count("<p")
+        "datetime": d.datetime.isoformat() if d.datetime else "NaN",
+        "number_of_img": d.html.count("<img") if d.html else "Nan",
+        "number_of_paragraphs": d.html.count("<p") if d.html else "Nan"
     })
 
 with open("/output-IM.json", "w", encoding="utf-8") as f:
