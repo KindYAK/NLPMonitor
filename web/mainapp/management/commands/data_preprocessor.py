4c7bdec687f2ce14dee7604025fd0a10ee555ff7
Yakunin Kirill <yakunin.k@mail.ru>
2019-10-15 11:12:26 +0600

    Preprocessing - delete kazakh

--- web/mainapp/management/commands/data_preprocessor.py
+++ web/mainapp/management/commands/data_preprocessor.py
@@ -12,10 +12,19 @@ class Command(BaseCommand):
     def preprocessing_raw_data(self):
         batch_size = 10000
 
-        qs = Document.objects.filter(id__gt=0)
+        def is_kazakh(text):
+            return sum([c in "ӘәҒғҚқҢңӨөҰұҮүІі" for c in text]) > 0.04
 
-        for batch in batch_qs(qs, batch_size=batch_size):
+        qs = Document.objects.filter(id__gt=0)
+        number_of_documents = qs.count()
+        for i, batch in enumerate(batch_qs(qs, batch_size=batch_size)):
+            print(f"Processing {i*batch_size}/{number_of_documents}")
             for doc in batch:
                 doc.text = BeautifulSoup(doc.text, "html.parser").text.strip().replace('\n', '')
                 doc.title = BeautifulSoup(doc.title, "html.parser").text.strip().replace('\n', '')
                 Document.objects.bulk_update(batch, fields=['text', 'title'])
+        for i, batch in enumerate(batch_qs(qs, batch_size=batch_size)):
+            print(f"Deleting {i*batch_size}/{number_of_documents}")
+            for doc in batch:
+                if is_kazakh(doc.text + doc.title):
+                    doc.delete()
