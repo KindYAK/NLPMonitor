60f3c370f86c55cc54467a68ec422b74ab9c4730
Yakunin Kirill <yakunin.k@mail.ru>
2019-10-16 16:43:26 +0600

    Visartm - small static-related fix

--- visartm/django_settings/settings.py
+++ visartm/django_settings/settings.py
@@ -123,5 +123,5 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    # os.path.join(BASE_DIR, "staticfiles"),
 )
