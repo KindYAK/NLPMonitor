4c17e44003f14da2a7f5fa5e5d8cba5153f21880
Yakunin Kirill <yakunin.k@mail.ru>
2019-10-30 15:07:01 +0600

    Move to template-based caching where necessary

--- web/mainapp/urls.py
+++ web/mainapp/urls.py
@@ -11,13 +11,13 @@ urlpatterns = [
     path('accounts/login/', cache_page(60*60*8)(auth_views.LoginView.as_view(template_name="mainapp/login.html")), name='login'),
     path('accounts/logout/', cache_page(60*60*8)(auth_views.LogoutView.as_view(next_page="/")), name='logout'),
 
-    path('search/', cache_page(60*60*8)(SearchView.as_view(template_name="mainapp/search.html")), name='search'),
-    path('dashboard/', DashboardView.as_view(template_name="mainapp/dashboard.html"), name='dashboard'),
+    path('search/', SearchView.as_view(), name='search'),
+    path('dashboard/', DashboardView.as_view(), name='dashboard'),
     path('kibana_dashboard/', KibanaDashboardView.as_view(), name="kibana_dashboard"),
 
-    path('document_view/<document_id>/', cache_page(60*60*8)(DocumentDetailView.as_view()), name='document_view'),
+    path('document_view/<document_id>/', DocumentDetailView.as_view(), name='document_view'),
 
-    path('topics_list/', cache_page(60*60*8)(TopicsListView.as_view()), name="topics_list"),
-    path('topic_documents_list/<topic_modelling>/<topic_name>/', cache_page(60*60*8)(TopicDocumentListView.as_view()),
+    path('topics_list/', TopicsListView.as_view(), name="topics_list"),
+    path('topic_documents_list/<topic_modelling>/<topic_name>/', TopicDocumentListView.as_view(),
         name="topic_document_list"),
 ]
