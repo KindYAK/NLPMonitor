debd32ce41bc680429dc041b26cbb4429b2f9eac
Yakunin Kirill <yakunin.k@mail.ru>
2019-11-05 10:17:10 +0600

    Topics_list - filter+list fix

--- web/staticfiles/js/topics_list_search_filter.js
+++ web/staticfiles/js/topics_list_search_filter.js
@@ -13,37 +13,24 @@ function init_table(){
 $('#topicSearchInput').val("");
 var table = init_table();
 
-// Search by text input
-var optionsText = {
-    valueNames: ['topic-name', 'topic-words'],
-};
-var searchEngineText = new List('search-results', optionsText);
-
-typeHandler = function(e){
+// Search+Filter function
+function search_filter(){
     table.destroy();
-    searchEngineText.search(e.target.value);
-    table = init_table();
-};
-var searchInput = document.querySelector('#topicSearchInput');
-searchInput.addEventListener('input', typeHandler);
-searchInput.addEventListener('propertychange', typeHandler);
 
-// Search by group select
-$('#topicFilterInput').change(function(e){
-    table.destroy();
-    if(e.target.value === "-1"){
-        searchEngineGroup.filter();
+    searchEngine.search($('#topicSearchInput').val());
+    if($('#topicFilterInput').val() === "-1"){
+        searchEngine.filter();
     } else {
         topics = null;
         for (topic_group of topic_groups_list.my_groups) {
-            if (topic_group.name === e.target.value) {
+            if (topic_group.name === $('#topicFilterInput').val()) {
                 topics = topic_group.topics;
                 break;
             }
         }
         if(!topics) {
             for (topic_group of topic_groups_list.public_groups) {
-                if (topic_group.name === e.target.value) {
+                if (topic_group.name === $('#topicFilterInput').val()) {
                     topics = topic_group.topics;
                     break;
                 }
@@ -53,14 +40,23 @@ $('#topicFilterInput').change(function(e){
         for(topic of topics) {
             topic_ids.push(topic.topic_id);
         }
-        searchEngineGroup.filter(function(item) {
+        searchEngine.filter(function(item) {
             return topic_ids.indexOf(item._values['topic-id']) !== -1;
         });
     }
-    table = init_table();
-});
 
-var optionsGroups = {
-    valueNames: ['topic-id',],
+    table = init_table();
+}
+
+// Search by text input
+var optionsText = {
+    valueNames: ['topic-name', 'topic-words', 'topic-id', ],
 };
-var searchEngineGroup = new List('search-results', optionsGroups);
+var searchEngine = new List('search-results', optionsText);
+
+var searchInput = document.querySelector('#topicSearchInput');
+searchInput.addEventListener('input', search_filter);
+searchInput.addEventListener('propertychange', search_filter);
+
+// Search by group select
+$('#topicFilterInput').change(search_filter);
