0ec382c71cee468dfcf2fe339d943eae3caf086c
Yakunin Kirill <yakunin.k@mail.ru>
2019-11-13 17:21:01 +0600

    Small range selector fix

--- web/staticfiles/js/range_plots_management.js
+++ web/staticfiles/js/range_plots_management.js
@@ -1,5 +1,5 @@
 function run_range_plot_management(csrf_token) {
-    function relayout_dynamics(range_from, range_to, id_to_skip) {
+    function rerender_new_range(range_from, range_to, id_to_skip) {
         var plot_ids = ["topic_dynamics", "topic_dynamics_normal", "topic_dynamics_weight"];
         for (var plot_id of plot_ids) {
             if (id_to_skip && plot_id !== id_to_skip) {
@@ -16,8 +16,8 @@ function run_range_plot_management(csrf_token) {
         var main_plot = document.getElementById('topic_dynamics');
         var range_from = main_plot.layout.xaxis[0];
         var range_to = main_plot.layout.xaxis[1];
-        if (last_range_from && last_range_to && last_range_from !== range_from && last_range_to !== range_to) {
-            relayout_dynamics(0, 1, null);
+        if (last_range_from === null || last_range_to === null || last_range_from !== range_from || last_range_to !== range_to) {
+            rerender_new_range(0, 1, null);
         }
     }, 1111);
 
@@ -29,10 +29,10 @@ function run_range_plot_management(csrf_token) {
             var range_to = target.xaxis.range[1];
 
             n_redraw_plot += 1;
-            if (n_redraw_plot % 3 !== 0) {
+            if (n_redraw_plot === 0 || n_redraw_plot % 3 !== 0) {
                 return;
             }
-            relayout_dynamics(range_from, range_to, e.target.id);
+            rerender_new_range(range_from, range_to, e.target.id);
         }
     );
 }
