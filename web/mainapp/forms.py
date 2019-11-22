67d790e20147d8340c0db8a0b1e13c92f31d2585
Yakunin Kirill <yakunin.k@mail.ru>
2019-11-22 21:16:59 +0600

    TM list form - replace bigartm with tm

--- web/mainapp/forms.py
+++ web/mainapp/forms.py
@@ -33,7 +33,7 @@ class TopicChooseForm(forms.Form):
         #                                                     f"COH={round(tm.tau_coherence_phi, 1) if hasattr(tm, 'perplexity') else 'None'} "
         #                                            ) for tm in s.scan()]
 
-        topic_modellings = ((tm.name, f"{tm.name} - {tm.number_of_topics} топиков - {tm.number_of_documents} текстов - " +
+        topic_modellings = ((tm.name, f"{tm.name.replace('bigartm', 'tm')} - {tm.number_of_topics} топиков - {tm.number_of_documents} текстов - " +
                                                    (f"{tm.source} - " if hasattr(tm, 'source') and tm.source else f"Все СМИ ") +
                                                    (f"С {tm.datetime_from[:10]} - " if hasattr(tm, 'datetime_from') and tm.datetime_from else f"") +
                                                    (f"По {tm.datetime_to[:10]} - " if hasattr(tm, 'datetime_to') and tm.datetime_to else f"")
