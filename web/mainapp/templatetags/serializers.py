656837c01aab049c8fe862d8d1b2c9fa32c460a0
Yakunin Kirill <yakunin.k@mail.ru>
2019-12-05 18:12:43 +0600

    Plots - round floats to 3 decimal points

--- web/mainapp/templatetags/serializers.py
+++ web/mainapp/templatetags/serializers.py
@@ -3,6 +3,12 @@ from django.template import Library
 
 register = Library()
 
+
 @register.filter
 def json(queryset):
     return serialize('json', queryset)
+
+
+@register.filter
+def round_fl(val, points):
+    return round(float(val), points)
