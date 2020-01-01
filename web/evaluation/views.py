ebad724aaf1901e99f8cdec037dca0b8d4bb67e2
Yakunin Kirill <yakunin.k@mail.ru>
2020-01-02 02:09:16 +0600

    Criterion eval - fix when no tm-criterion index exists

--- web/evaluation/views.py
+++ web/evaluation/views.py
@@ -32,6 +32,8 @@ class CriterionEvalAnalysisView(TemplateView):
         context['granularity'] = self.request.GET['granularity'] if 'granularity' in self.request.GET else "1w"
         context['smooth'] = True if 'smooth' in self.request.GET else (True if 'granularity' not in self.request.GET else False)
         context['topic_modelling'] = self.request.GET['topic_modelling'] if 'topic_modelling' in self.request.GET else context['topic_modellings'][0][0]
+        indices = ES_CLIENT.indices.get_alias(f"{ES_INDEX_DOCUMENT_EVAL}_*").keys()
+        context['criterions_list'] = context['criterions_list'].filter(id__in=[index.split("_")[-1] for index in indices])
         context['public_groups'] = context['public_groups'].filter(topic_modelling_name=context['topic_modelling'])
         context['my_groups'] = context['my_groups'].filter(topic_modelling_name=context['topic_modelling'])
         context['criterions'] = EvalCriterion.objects.filter(id__in=self.request.GET.getlist('criterions')) \
