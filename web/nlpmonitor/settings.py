1ceb643ef601c34acff7d323f93aea1468a76091
Yakunin Kirill <yakunin.k@mail.ru>
2020-01-09 09:50:34 +0600

    Change ES_CLIENT to new ES cluster

--- web/nlpmonitor/settings.py
+++ web/nlpmonitor/settings.py
@@ -219,7 +219,9 @@ ES_PORT = os.getenv('DJANGO_ES_PORT', '9200')
 from elasticsearch import Elasticsearch
 ES_CLIENT = Elasticsearch(
     hosts=[
-        {'host': ES_HOST}
+        {'host': 'master1'},
+        {'host': 'master2'},
+        {'host': 'master3'},
     ],
     timeout=60,
     max_retries=100,
