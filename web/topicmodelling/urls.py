89d4792205a96ee7b08ca65f390c3f4c88eb7cc0
Sanzhar <sanzharmrz@gmail.com>
2020-02-19 12:20:48 +0600

    feat topic_modelling urls: create dynamic_tm route

--- web/topicmodelling/urls.py
+++ web/topicmodelling/urls.py
@@ -1,15 +1,14 @@
-from django.urls import path, include
+from django.urls import path
+
 from .views import *
-from django.views.generic import TemplateView
-from django.contrib.auth import views as auth_views
-from django.views.decorators.cache import cache_page
 
 app_name = 'topicmodelling'
 
 urlpatterns = [
     path('topics_list/', TopicsListView.as_view(), name="topics_list"),
     path('topic_documents_list/<topic_modelling>/<topic_name>/', TopicDocumentListView.as_view(),
-        name="topic_document_list"),
+         name="topic_document_list"),
     path('topic_documents_list/<topic_modelling>/', TopicDocumentListView.as_view(),
-        name="topics_group_document_list"),
+         name="topics_group_document_list"),
+    path('dynamic_tm/', DynamicTMView.as_view(), name="dynamic_tm_view")
 ]
