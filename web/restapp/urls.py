906d27b6c79482fd18d08e99596608601bd648f5
Sanzhar <sanzharmrz@gmail.com>
2020-02-28 18:56:36 +0600

    refactor restapp/urls: remove unused routing dynamic_tm, because of html processing instead of restapp

--- web/restapp/urls.py
+++ web/restapp/urls.py
@@ -8,7 +8,6 @@ router.register("topic_group", TopicGroupViewSet, basename="topic_group")
 router.register("criterion_eval", CriterionEvalViewSet, basename="criterion_eval")
 router.register("range_documents", RangeDocumentsViewSet, basename="range_documents")
 router.register("criterion_eval_util", CriterionEvalUtilViewSet, basename="criterion_eval_util")
-router.register("dynamic_tm", DynamicTMViewSet, basename="dynamic_tm")
 
 app_name = 'restapp'
 
