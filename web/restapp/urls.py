9fa231d4b7be9b229269134edc6aa440f3f5fcfb
Sanzhar <sanzharmrz@gmail.com>
2020-02-28 18:00:47 +0600

    feat restapp/urls: register new route dynamic_tm

--- web/restapp/urls.py
+++ web/restapp/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from rest_framework import routers
+
 from restapp.views import *
 
 router = routers.DefaultRouter()
@@ -7,6 +8,7 @@ router.register("topic_group", TopicGroupViewSet, basename="topic_group")
 router.register("criterion_eval", CriterionEvalViewSet, basename="criterion_eval")
 router.register("range_documents", RangeDocumentsViewSet, basename="range_documents")
 router.register("criterion_eval_util", CriterionEvalUtilViewSet, basename="criterion_eval_util")
+router.register("dynamic_tm", DynamicTMViewSet, basename="dynamic_tm")
 
 app_name = 'restapp'
 
