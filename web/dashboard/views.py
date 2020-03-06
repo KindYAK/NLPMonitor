b83fe57c5c4bc459c76341b48beea8ff20f21530
Yakunin Kirill <yakunin.k@mail.ru>
2020-03-06 12:00:16 +0600

    Dashboard - rights

--- web/dashboard/views.py
+++ web/dashboard/views.py
@@ -14,6 +14,11 @@ class DashboardView(TemplateView):
             context['dashboard_template'] = DashboardPreset.objects.first()
         else:
             context['dashboard_template'] = get_object_or_None(DashboardPreset, id=dashboard_id)
+        if not self.request.user.is_superuser:
+            group = (hasattr(self.request.user, "expert") and self.request.user.expert.group) or \
+                    (hasattr(self.request.user, "viewer") and self.request.user.viewer.group)
+            if not group or context['dashboard_template'] not in group.dashboard_presets.all():
+                return context
         if not context['dashboard_template']:
             return context
         context['widgets'] = context['dashboard_template'].widgets.all().order_by('index')
