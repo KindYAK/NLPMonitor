b0867da0645067a36d2bcdf8fbfbe8296379a410
Yakunin Kirill <yakunin.k@mail.ru>
2020-03-10 16:26:55 +0600

    Refactoring

--- web/mainapp/management/commands/el_reindex.py
+++ web/mainapp/management/commands/el_reindex.py
@@ -1,10 +1,10 @@
-
-from django.core.management.base import BaseCommand
-from elasticsearch import Elasticsearch
 from time import sleep
+
 from django.conf import settings
+from django.core.management.base import BaseCommand
 
 es = settings.ES_CLIENT
+
 
 def shards_mapping(doc_count: int) -> int:
     if isinstance(doc_count, str):
@@ -14,6 +14,8 @@ def shards_mapping(doc_count: int) -> int:
         return 5
     elif doc_count > 1_000_000:
         return 3
+    elif doc_count > 100_000:
+        return 2
     else:
         return 1
 
@@ -70,13 +72,13 @@ class Command(BaseCommand):
 
         for key, (pri, rep, docs_count) in indexes.items():
             if key.startswith(search_pattern):
-                new_index = 'temp_'+key
+                new_index = 'temp_' + key
                 # sometimes indexes name is cached
                 if docs_count is None:
                     continue
                 # just in case
-                if es.indices.exists('temp_'+new_index):
-                    es.indices.delete(index='temp_'+new_index)
+                if es.indices.exists('temp_' + new_index):
+                    es.indices.delete(index='temp_' + new_index)
                     continue
 
                 if es.indices.exists(new_index):
