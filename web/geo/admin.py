ea566754f704635c883865d0b08a1f55284323e5
Sanzhar <sanzharmrz@gmail.com>
2020-04-08 14:22:19 +0600

    feat admin: add 3 admin classes

--- web/geo/admin.py
+++ web/geo/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from geo.models import Locality, District, Area
+
+
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ('area_name',)
+    list_filter = ()
+    search_fields = ('area_name',)
+
+
+class DistrictAdmin(admin.ModelAdmin):
+    list_display = ('district_name',)
+    list_filter = ()
+    search_fields = ('district_name',)
+
+
+class LocalityAdmin(admin.ModelAdmin):
+    list_display = ('locality_name', 'kato_code', 'district', 'latitude', 'longitude')
+    list_filter = ()
+    search_fields = ('locality_name',)
+
+
+admin.site.register(Area, AreaAdmin)
+admin.site.register(District, DistrictAdmin)
+admin.site.register(Locality, LocalityAdmin)
