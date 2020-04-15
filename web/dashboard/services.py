e4a055a2d3a7e96c310c0419699e0bec93d33e57
Sanzhar <sanzharmrz@gmail.com>
2020-04-15 22:24:25 +0600

    feat services: add new location search factory and refactor eval search factory

--- web/dashboard/services.py
+++ web/dashboard/services.py
@@ -2,12 +2,27 @@ from datetime import datetime, timedelta
 
 from elasticsearch_dsl import Search
 
-from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT_EVAL
+from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT_EVAL, ES_INDEX_DOCUMENT_LOCATION
 from .util import default_parser
 
 
 def es_document_eval_search_factory(widget, **kwargs):
     s = Search(using=ES_CLIENT, index=f"{ES_INDEX_DOCUMENT_EVAL}_{widget.topic_modelling_name}_{widget.criterion.id}")
+    s = es_default_fields_parser(widget, s)
+    return s
+
+
+def es_document_location_search_factory(widget, **kwargs):
+    s = Search(using=ES_CLIENT, index=ES_INDEX_DOCUMENT_LOCATION)
+    s = es_default_fields_parser(widget, s)
+    params = widget.params_obj
+    if params:
+        for key, value in params.items():
+            s = s.filter('term', **{key: value})
+    return s
+
+
+def es_default_fields_parser(widget, s):
     datetime_from = datetime(2000, 1, 1).date()
     datetime_to = datetime.now().date()
 
@@ -35,3 +50,5 @@ def es_document_eval_search_factory(widget, **kwargs):
             parent_search=s
         )
     return s
+
+
