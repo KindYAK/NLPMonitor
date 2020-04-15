2940d9cd4fa058ba7f26d5dd007b6f731a076c30
Kirill Yakunin <yakunin.k@mail.ru>
2020-04-15 15:00:41 +0600

    push - comment pushing

--- push.sh
+++ push.sh
@@ -2,11 +2,11 @@
 set -e
 cd web
 docker build . -f docker/Dockerfile -t vm-registry.ipic.kz/nlpmonitor:prod
-docker login vm-registry.ipic.kz
-docker push vm-registry.ipic.kz/nlpmonitor:prod
+#docker login vm-registry.ipic.kz
+#docker push vm-registry.ipic.kz/nlpmonitor:prod
 
 cd ..
 cd airflow-worker
 docker build . -f Dockerfile -t vm-registry.ipic.kz/airflow-worker:prod
-docker login vm-registry.ipic.kz
-docker push vm-registry.ipic.kz/airflow-worker:prod
+#docker login vm-registry.ipic.kz
+#docker push vm-registry.ipic.kz/airflow-worker:prod
