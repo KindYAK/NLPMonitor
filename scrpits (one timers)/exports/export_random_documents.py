7275bc4fa959f30967c95a788b094765f0b9cb23
Yakunin Kirill <yakunin.k@mail.ru>
2020-05-04 17:58:44 +0600

    feat - script - export sample - corpus and date filtering, joining document_evals

--- scrpits (one timers)/exports/export_random_documents.py
+++ scrpits (one timers)/exports/export_random_documents.py
@@ -1,30 +1,75 @@
+import csv
+import datetime
 import json
 
-from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT
+from elasticsearch_dsl import Search
 
-res = ES_CLIENT.search(index=ES_INDEX_DOCUMENT, size=50000, body={
+from nlpmonitor.settings import ES_CLIENT, ES_INDEX_DOCUMENT, ES_INDEX_DOCUMENT_EVAL
+
+corpus = "main"
+res = ES_CLIENT.search(index=ES_INDEX_DOCUMENT, size=2000, body={
     "query": {
         "function_score": {
             "functions": [
                 {
                     "random_score": {
-                        "seed": "saoddf;oj3049uf3049uf3049fu034f"
+                        "seed": "294j5bw[oamvrin"
                     }
                 }
-            ]
+            ],
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "term": {
+                                "corpus": corpus
+                            },
+                        },
+                        {
+                            "range": {
+                                "datetime": {
+                                    "gte": datetime.datetime(2018, 1, 1)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
         }
-    },
+    }
 })
+
+tm = "bigartm_two_years_rus_and_rus_propaganda"
+criterion_id = 34
+
+s = Search(using=ES_CLIENT, index=f"{ES_INDEX_DOCUMENT_EVAL}_{tm}_{criterion_id}")
+s = s.filter('terms', document_es_id=[hit['_id'] for hit in res['hits']['hits']])
+s = s.source(('document_es_id', 'value', ))
+
+document_eval_dict = dict(
+    (hit.document_es_id, hit.value) for hit in s.scan()
+)
 
 output = []
 for hit in res['hits']['hits']:
+    if hit['_id'] not in document_eval_dict:
+        continue
     output.append(
         {
             "id": hit['_source']['id'],
             "title": hit['_source']['title'],
-            "text": hit['_source']['text'],
+            "datetime": hit['_source']['datetime'],
+            "source": hit['_source']['source'],
+            "url": hit['_source']['url'],
+            "value": document_eval_dict[hit['_id']],
         }
     )
 
-with open("/output.json", "w", encoding="utf-8") as f:
-    f.write(json.dumps(output))
+# with open("/output_propa.json", "w", encoding="utf-8") as f:
+#     f.write(json.dumps(output))
+
+keys = output[0].keys()
+with open(f'/output2.csv', 'w') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(output)
