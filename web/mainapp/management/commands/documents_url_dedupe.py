891160bc8fda2cb7104dd304ba537f5870d5b0e5
Yakunin Kirill <yakunin.k@mail.ru>
2020-05-21 23:17:06 +0600

    fix - documents_url_dedupe - optimization

--- web/mainapp/management/commands/documents_url_dedupe.py
+++ web/mainapp/management/commands/documents_url_dedupe.py
@@ -9,16 +9,12 @@ class Command(BaseCommand):
         unique_collisions = 0
         total = Document.objects.count()
         for doc in Document.objects.all().only("url"):
-            try:
-                doc_dupe = Document.objects.exclude(id=doc.id).filter(url=doc.url)
-            except:
-                doc_dupe = None
-            if doc_dupe:
+            doc_dupe = Document.objects.exclude(id=doc.id).filter(url=doc.url)
+            for d in doc_dupe:
                 unique_collisions += 1
                 if unique_collisions % 10000 == 0:
                     print("Deleting duplicate", unique_collisions)
-                for d in doc_dupe:
-                    d.delete()
+                d.delete()
             i += 1
             if i % 100000 == 0:
                 print(i, "/", total)
