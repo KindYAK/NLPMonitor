7a159f56b2ddac3b95715a87017d9e3bb3948df6
Yakunin Kirill <yakunin.k@mail.ru>
2020-05-23 16:32:40 +0600

    fix - documents_url_dedupe - optimization - rewritten

--- web/mainapp/management/commands/documents_url_dedupe.py
+++ web/mainapp/management/commands/documents_url_dedupe.py
@@ -1,3 +1,6 @@
+import datetime
+from collections import defaultdict
+
 from django.core.management.base import BaseCommand
 
 from mainapp.models import *
@@ -9,13 +12,19 @@ class Command(BaseCommand):
         unique_collisions = 0
         qs = Document.objects.exclude(url=None)
         total = qs.count()
+        url_ids_dict = defaultdict(set)
+        print("!!!", "Getting url_ids_dict", datetime.datetime.now())
         for doc in qs.only("url"):
-            doc_dupe = Document.objects.exclude(id=doc.id).filter(url=doc.url)
-            for d in doc_dupe:
-                unique_collisions += 1
-                if unique_collisions % 10000 == 0:
-                    print("Deleting duplicate", unique_collisions)
-                d.delete()
+            url_ids_dict[doc.url.strip()].add(doc.id)
             i += 1
             if i % 100000 == 0:
-                print(i, "/", total)
+                print(i, "/", total, "to dict", datetime.datetime.now())
+
+        for ids in url_ids_dict.values():
+            ids_to_delete = list(ids)[1:]
+            if not ids_to_delete:
+                continue
+            unique_collisions += len(ids_to_delete)
+            Document.objects.filter(id__in=ids_to_delete).delete()
+            if unique_collisions % 10000 == 0:
+                print("Deleting duplicate", unique_collisions)
