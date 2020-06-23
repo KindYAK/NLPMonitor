0d23da97926af66af69aa163e0052c467ff74dee
Yakunin Kirill <yakunin.k@mail.ru>
2020-06-23 22:16:34 +0600

    change - apertium container - increase max request header size (for larger texts)

--- containers/apertium/waitress_serve.py
+++ containers/apertium/waitress_serve.py
@@ -2,4 +2,4 @@ from waitress import serve
 import appertium_server
 
 
-serve(appertium_server.app, host='0.0.0.0', port=8005, max_request_header_size=8 * 1024 * 1024)
+serve(appertium_server.app, host='0.0.0.0', port=8005, max_request_header_size=1024 * 1024 * 1024)
