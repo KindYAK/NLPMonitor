e8d7ad93eb886fe350e85cf71ca5ff70081f5585
Yakunin Kirill <yakunin.k@mail.ru>
2020-06-26 14:01:49 +0600

    feat - apertium container - kazakh stopwords list

--- containers/apertium/apertium-kaz/appertium_server.py
+++ containers/apertium/apertium-kaz/appertium_server.py
@@ -7,13 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     text = request.args.get('text')
-    lemmatized = subprocess.getoutput(f"echo '{text}' | apertium -d . kaz-disam | python3 /kaz-tagger/kaz-tagger.py")
+    lemmatized = subprocess.getoutput(f"echo '{text}' | apertium -d . kaz-disam | python3 /kaz-tagger/kaz_tagger.py")
     lines = [l.strip() for l in lemmatized.split("\n")]
+
+    with open("stopwords.txt", "r") as f:
+        stopwords = [l.strip() for l in f.readlines()]
+
     result = ""
     for i, line in enumerate(lines):
         if line.startswith("\"<") and line.endswith(">\""):
-            token = lines[i+1].split("\"")[1].replace("*", "").strip()
+            token = lines[i+1].split("\"")[1].replace("*", "").strip().lower()
             if not token.isalpha():
+                continue
+            if token in stopwords:
                 continue
             result += token + " "
     return {
