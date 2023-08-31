a1de96b7b301d6b7365ebc2947be4d5d13b0983e
KindYAK <yakunin.k@mail.ru>
2023-08-31 20:24:56 +0600

    Temp fix

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 set -e
 cd web
-docker build . -f docker/Dockerfile -t vm-registry.ipic.kz/nlpmonitor:prod
+docker build . -f docker/Dockerfile -t nlpmonitor:prod
 #docker login vm-registry.ipic.kz
 #docker push vm-registry.ipic.kz/nlpmonitor:prod
 
-cd ..
-cd airflow-worker
-docker build . -f Dockerfile -t vm-registry.ipic.kz/airflow-worker:prod
+#cd ..
+#cd airflow-worker
+#docker build . -f Dockerfile -t airflow-worker:prod
 #docker login vm-registry.ipic.kz
 #docker push vm-registry.ipic.kz/airflow-worker:prod
